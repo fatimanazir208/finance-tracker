758b0880d83f24d5cdacbd13915588d5fca65ef3
Fatima Nazir <fatima@deltalabs.io>
2023-02-03 15:09:56 +0500

    Final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'my_friends', to: 'users#my_friends'
   root to: 'welcome#index'
   devise_for :users
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
